0f4b29d62dc0a797286c7878dce5e878a0246265
heshengyx <heshengyx@163.com>
2016-12-31 08:45:33 +0800

    1

--- src/main/java/com/grgbanking/electric/dao/redis/impl/UserDaoImpl.java
+++ src/main/java/com/grgbanking/electric/dao/redis/impl/UserDaoImpl.java
@@ -4,46 +4,66 @@ import java.util.List;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
 import com.grgbanking.electric.dao.IUserDao;
 import com.grgbanking.electric.entity.User;
 import com.grgbanking.electric.param.UserQueryParam;
+import com.grgbanking.electric.transcoder.ListTranscoder;
+import com.grgbanking.electric.transcoder.ObjectsTranscoder;
 
 @Repository
 public class UserDaoImpl extends RedisBaseDao<String, User> implements IUserDao {
 
 	@Override
 	public int save(final User user) {
-		boolean result = redisTemplate.execute(new RedisCallback<Boolean>() {
+		/*boolean result = redisTemplate.execute(new RedisCallback<Boolean>() {
 			public Boolean doInRedis(RedisConnection connection)
 					throws DataAccessException {
 				RedisSerializer<String> serializer = getRedisSerializer();
-				byte[] key = serializer.serialize(user.getId());
-				byte[] name = serializer.serialize(user.getName());
+				//byte[] key = serializer.serialize(user.getId());
+				//byte[] name = serializer.serialize(user.getName());
+				
+				byte[] key = user.getId().getBytes();
+				byte[] name = SerializationUtil.serialize(user);
 				return connection.setNX(key, name);
 			}
-		});
+		});*/
+		ValueOperations<String, User> valueOpt = redisTemplate.opsForValue();
+		valueOpt.set(user.getId(), user);
 		return 0;
 	}
 
 	@Override
-	public int saveBatch(List<User> users) {
-		// TODO Auto-generated method stub
+	public int saveBatch(final List<User> users) {
+		/*boolean result = redisTemplate.execute(new RedisCallback<Boolean>() {
+			public Boolean doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				//ObjectsTranscoder<User> serializer = new ObjectsTranscoder<User>();
+				//RedisSerializer<String> serializer = getRedisSerializer();
+				byte[] key = "zhangsan".getBytes();
+				for (User user : users) {
+					System.out.println(user.getName());
+				}
+				//byte[] name = serializer.serialize(users);
+				byte[] name = ListTranscoder.serialize(users);
+				return connection.setNX(key, name);
+			}
+		});*/
+		ListOperations<String, User> valueOpt = redisTemplate.opsForList();
 		return 0;
 	}
 
 	@Override
 	public int update(User user) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int deleteById(String id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
@@ -61,11 +81,11 @@ public class UserDaoImpl extends RedisBaseDao<String, User> implements IUserDao 
 
 	@Override
 	public User getById(final String id) {
-		return redisTemplate.execute(new RedisCallback<User>() {
+		/*return redisTemplate.execute(new RedisCallback<User>() {
 			public User doInRedis(RedisConnection connection)
 					throws DataAccessException {
 				RedisSerializer<String> serializer = getRedisSerializer();
-				byte[] key = serializer.serialize(id);
+				//byte[] key = serializer.serialize(id);
 				byte[] value = connection.get(key);
 				if (value == null) {
 					return null;
@@ -74,9 +94,13 @@ public class UserDaoImpl extends RedisBaseDao<String, User> implements IUserDao 
 				User user = new User();
 				user.setId(id);
 				user.setName(name);
+				byte[] key = id.getBytes();
+				User user = (User) SerializationUtil.deserialize(key);
 				return user;
 			}
-		});
+		});*/
+		ValueOperations<String, User> valueOpt = redisTemplate.opsForValue();
+		return valueOpt.get(id);
 	}
 
 	@Override
@@ -99,8 +123,10 @@ public class UserDaoImpl extends RedisBaseDao<String, User> implements IUserDao 
 
 	@Override
 	public List<User> queryAll(UserQueryParam param) {
-		// TODO Auto-generated method stub
-		return null;
+		ObjectsTranscoder<User> serializer = new ObjectsTranscoder<User>();
+		byte[] key = "zhangsan".getBytes();
+		List<User> list = (List<User>) ListTranscoder.deserialize(key);
+		return list;
 	}
 
 }
--- src/test/java/com/grgbanking/electric/test/UserRedisTest.java
+++ src/test/java/com/grgbanking/electric/test/UserRedisTest.java
@@ -1,10 +1,14 @@
 package com.grgbanking.electric.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.grgbanking.electric.dao.IUserDao;
 import com.grgbanking.electric.entity.User;
+import com.grgbanking.electric.util.UUIDGeneratorUtil;
 
 public class UserRedisTest extends BaseRedisTest {
 
@@ -14,14 +18,34 @@ public class UserRedisTest extends BaseRedisTest {
 	@Test
 	public void testSave() {
 		User user = new User();
-		user.setId("8F3EC9CA888F4FF0B5686E2D4FC7F42D");
-		user.setName("李四");
+		user.setId("7F3EC9CA888F4FF0B5686E2D4FC7F42A");
+		user.setName("王五");
 		userDao.save(user);
 	}
 	
 	@Test
+	public void testSaveList() {
+		List<User> users = new ArrayList<User>();
+		for (int i = 0; i < 5; i++) {
+			User user = new User();
+			user.setId(UUIDGeneratorUtil.getUUID());
+			user.setName("李四" + i);
+			users.add(user);
+		}
+		userDao.saveBatch(users);
+	}
+	
+	@Test
+	public void testGetList() {
+		List<User> users = userDao.queryAll(null);
+		for (User user : users) {
+			System.out.println(user.getName());
+		}
+	}
+	
+	@Test
 	public void testGetById() {
-		User user = userDao.getById("8F3EC9CA888F4FF0B5686E2D4FC7F42D");
+		User user = userDao.getById("7F3EC9CA888F4FF0B5686E2D4FC7F42A");
 		System.out.println(user.getName());
 	}
 }
